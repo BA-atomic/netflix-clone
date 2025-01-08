a155dcf8c1ed2f593e4f1459b4e43416642d2188
Bamigbele Isaiah Ayomide <aryomi5528@gmail.com>
2025-01-08 21:34:48 +0100

    fourth commit- fetching movies from the API

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-async function fetch() {
-  const options = {
-    method: 'GET',
-    url: 'https://moviedatabase8.p.rapidapi.com/Random/20',
-    headers: {
-      'x-rapidapi-key': '00dcf8ad1bmshfba48a0565371afp170166jsnaa148f32f351',
-      'x-rapidapi-host': 'moviedatabase8.p.rapidapi.com',
-    },
-  };
+async function fetchData() {
+  const randomNumber = Math.floor(Math.random() * 500) + 1;
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${randomNumber}&sort_by=popularity.desc`,
+    {
+      params: {
+        api_key: '45601c7960258aafebbf8ed4f27131c6',
+      },
+    }
+  );
 
-  try {
-    const response = await axios.request(options);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
+  if (response.data.Error) {
+    return [];
   }
+
+  const allMovies = response.data.results;
+  const tenMovies = getTenRandomMovies(allMovies, 10);
+  console.log(tenMovies);
 }
-fetch();
+fetchData();
+
+function getTenRandomMovies(array, size) {
+  const result = [];
+  const tempResult = [...array];
+
+  for (let i = 0; i < size; i++) {
+    randomIndex = Math.floor(Math.random() * result.length);
+    result.push(tempResult[randomIndex]);
+    tempResult.splice(randomIndex, 1); //remove the selected random index
+  }
+
+  return result;
+}
